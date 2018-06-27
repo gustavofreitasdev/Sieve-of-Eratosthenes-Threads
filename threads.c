a76796c2972648f676e34584b52a8a61bcfb5963
Gustavo F Silva <gustavofsilva.dev@gmail.com>
2018-06-27 03:53:08 -0300

    Ajustando variaveis globais compartilhadas, e criando Thread para mostrar resultado.

--- threads.c
+++ threads.c
@@ -1,7 +1,36 @@
 #include "threads.h"
 
-void *principal(void *param){}
+void *principal(void *param) {}
 
-void *siege(void *param){}
+void *siege(void *param) {}
 
-void *mostrarResultado(void *param){}
+void *resultado(void *param)
+{
+    Resposta resultado;
+    while (1)
+    {
+        /* Continua no laço até Thread que a chamou uni-lá */
+        /* INICIO SEÇÃO CRÍTICA */
+        pthread_mutex_lock(&gBloqueioMemoriaCompartilhada);
+            while (resposta.resultado != -1)
+            {
+                /* Continua no laço e fica na espera até o resultado da resposta seja um valor válido */
+                pthread_cond_wait(&gResultado, &gBloqueioMemoriaCompartilhada);
+            }
+            resposta.resultado = -1; /* indica que o resultado já está sendo consumido */
+            qntdNumerosCalculados++; /* atualiza quantos números já foram calculados */
+            resultado = resposta; /* cria uma váriavel para imprimir o resultado, para liberar a zona crítiza o mais rápido possível */
+        pthread_mutex_unlock(&gBloqueioMemoriaCompartilhada);
+        /* FIM SEÇÃO CRÍTICA */
+
+        if (resultado.ePrimo == 1)
+        {
+            printf("%d is prime (stored in sieve %d)", resultado.resultado, resultado.siege); /* caso o número for primo */
+        }
+        else
+        {
+            printf("%d divided by %d at sieve %d", resultado.resultado, resultado.divisor, resultado.siege); /* caso o número não for primo */
+        }
+        fflush(stdout);
+    }
+}
--- threads.h
+++ threads.h
@@ -3,24 +3,29 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-/* TADS */
+/* Tads */
 typedef struct tResposta
 {
-    char *mensagem;
+    unsigned resultado;
+    unsigned divisor;
+    short ePrimo;
     int siege;
 } Resposta;
 
-/* CONSTANTES */
+/* Constantes */
 
 /* Dadois Compartilhados Globais */
 Resposta resposta;
 resposta.siege = -1;
+unsigned int qntdNumerosCalculados = 0;
 
+/* Dados das Threads */
 pthread_mutex_t gBloqueioMemoriaCompartilhada = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t gResultado = PTHREAD_COND_INITIALIZER;
 pthread_cond_t gSieges = PTHREAD_COND_INITIALIZER;
 pthread_cond_t gBuffer = PTHREAD_COND_INITIALIZER;
 
+/* Informações sobre as Threads */
 void *principal(void *param);
 void *siege(void *param);
 void *mostrarResultado(void *param);
