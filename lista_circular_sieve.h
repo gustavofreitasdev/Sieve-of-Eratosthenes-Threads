35e6380028346fab6fff89d9a9dc8fd2a31536aa
Gustavo F Silva <gustavofsilva.dev@gmail.com>
2018-06-27 20:57:48 -0300

    Adicionando comentários

--- lista_circular_sieve.h
+++ lista_circular_sieve.h
@@ -32,15 +32,16 @@ void liberarLista(ListaCircularSieve *lista);
 void bloquearSieve(ListaCircularSieve *sieves, unsigned posicao);
 /* Função responsável por desbloquear uma sieve, dada a Lista Circular Sieve e a posição do Sieve */
 void desbloquearSieve(ListaCircularSieve *sieves, unsigned posicao);
+/* Função responsável por mudar valor a ser testado de um Sieve, dada uma Lista Circular Sieve, a posição do Sieve e o novo valor */
+void setValorSerTestado(ListaCircularSieve *sieves, unsigned posicao, unsigned novoValor);
+/* Função responsável por imprimir uma Lista Circular Sieve */
+void imprimirLista(ListaCircularSieve *sieves);
+
 /* Função responsável por verificar se uma Sieve está disponível, da uma Lista Circular Sieve e a posição do Sieve */
 short verificarSieveDisponivel(ListaCircularSieve *sieve, unsigned posicao);
 /* Função responsável por verificar se uma Sieve está disponível para cálculo, da uma Lista Circular Sieve e a posição do Sieve */
 short verificarSieveEstaDisponivelCalculo(ListaCircularSieve *sieves, unsigned posicao);
-/* Função responsável por mudar valor a ser testado de um Sieve, dada uma Lista Circular Sieve, a posição do Sieve e o novo valor */
-void setValorSerTestado(ListaCircularSieve *sieves, unsigned posicao, unsigned novoValor);
 /* Função responsável por adicionar elementos de forma balanceada na Lista Circular Sieve, dado a Lista Circular e o novo elemento a ser adicionado */
 short adicionarElementoLista(ListaCircularSieve *sieves, unsigned elemento);
-/* Função responsável por imprimir uma Lista Circular Sieve */
-void imprimirLista(ListaCircularSieve *sieves);
 /* Função responsável por verificar se existe algum elemento divisível (valores guardados em valoresTestados), dado uma Sieve e um elemento a ser comparado */
 short verificarSeElementosSaoDivisiveis(Sieve sieve, unsigned elementoComparado);
