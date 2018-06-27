41b64d1bdd7bd27f8aca86c55cd5958c0ba30f5d
Gustavo F Silva <gustavofsilva.dev@gmail.com>
2018-06-27 02:13:38 -0300

    Ajustando lista circular e adicionando funções

---
+++ lista_circular_sieve.c
@@ -0,0 +1,109 @@
+#include "lista_circular_sieve.h"
+
+ListaCircularSieve *criarLista(int tamanho)
+{
+    ListaCircularSieve *resultado = (ListaCircularSieve *)malloc(sizeof(ListaCircularSieve)); /* tenta alocar espaço para lista circular */
+    if (resultado)
+    {
+        /* Caso consiga alocar espaço para lista */
+        resultado->tamanhoMaximo = tamanho;                         /* define tamanho da lista */
+        resultado->anel = (Sieve *)malloc(sizeof(Sieve) * tamanho); /* tenta alocar espaço para amazenar anel contendo Sieve */
+        if (resultado->anel)
+        {
+            /* Caso consiga alocar espaço para anel */
+            for (int cursor = 0; cursor < tamanho; cursor++)
+            {
+                /* Define e inicializa cada Sieve no anel da Lista Circular */
+                Sieve sieve;
+                sieve.valoresTestados = (unsigned *)malloc(sizeof(unsigned) * BUFFER_SIEVE); /* tenta alocar lista cotendo valores testados */
+                if (sieve.valoresTestados)
+                {
+                    /* Caso consiga alocar lista contento valores a ser testados */
+                    sieve.posicao = cursor; /* posicao que o Sieve está no anel */
+
+                    sieve.valorSerTestado = 0;
+                    sieve.qntdValoresTestados = 0;
+                    sieve.bloqueado = DESBLOQUEADO;
+                    resultado->anel[cursor] = sieve;
+                }
+                else
+                {
+                    /* Caso não consiga alocar lista para valores a ser testados */
+                    printf("[erro] ao alocar espaço para lista de valores a ser testados (sieve)\n");
+                    fflush(stdout);
+                    free(resultado->anel); /* libera memória alocada para anel */
+                    free(resultado);       /* libera memória alocada para o resultado */
+                }
+            }
+        }
+        else
+        {
+            /* Caso não consiga alocar espaço para anel */
+            printf("[erro] ao alocar espaço para anel (lista circular sieve)\n");
+            fflush(stdout);
+            free(resultado); /* libera a memória alocada para o resultado */
+        }
+    }
+
+    return resultado;
+}
+void liberarLista(ListaCircularSieve *lista)
+{
+    free(lista->anel);
+    free(lista);
+}
+
+void bloquearSieve(ListaCircularSieve *sieves, unsigned posicao)
+{
+    if (sieves && posicao < sieves->tamanhoMaximo && sieves->anel[posicao].bloqueado == 0)
+        sieves->anel[posicao].bloqueado = 1;
+}
+short verificarSieveDisponivel(ListaCircularSieve *sieves, unsigned posicao){
+    short disponivel = DESBLOQUEADO;
+    if(sieves && posicao < sieves->tamanhoMaximo){
+        disponivel = sieves->anel[posicao].bloqueado;        
+    }
+    return disponivel;
+}
+
+short adicionarElementoLista(ListaCircularSieve *sieves, unsigned posicao, unsigned elemento){
+    short elementoAdicionado = 0;
+    if(sieves && posicao < sieves->tamanhoMaximo){
+        Sieve sieve = sieves->anel[posicao];
+        if(sieve.qntdValoresTestados < BUFFER_SIEVE){
+            sieves->anel[posicao].valoresTestados[sieve.qntdValoresTestados] = elemento;
+            sieves->anel[posicao].qntdValoresTestados++;
+            elementoAdicionado = 1;
+        }
+    }
+
+    return elementoAdicionado;
+}
+
+void imprimirLista(ListaCircularSieve *sieves)
+{
+    Sieve sieve;
+    unsigned *anel;
+    for (int cursor = 0; cursor < sieves->tamanhoMaximo; cursor++)
+    {
+        sieve = sieves->anel[cursor];
+        if (sieve.bloqueado == BLOQUEADO)
+            printf("[sieve %d] valor ser testado %d - bloqueado\n", cursor, sieves->anel[cursor].valorSerTestado);
+        else
+            printf("[sieve %d] valor ser testado %d - desbloqueado\n", cursor, sieves->anel[cursor].valorSerTestado);
+        fflush(stdout);
+
+
+        anel = sieve.valoresTestados;
+        for(int cursor2=0; cursor2<sieve.qntdValoresTestados; cursor2++){
+            printf("%d ", anel[cursor2]);
+            fflush(stdout);
+        }
+        printf("\n");
+        fflush(stdout);
+    }
+}
+
+short verificarSeElementosSaoDivisiveis(Sieve sieve, unsigned elementoComparado){
+    
+}
--- lista_circular_sieve.h
+++ lista_circular_sieve.h
@@ -1,35 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define BUFFER_SIEVE 10
+#define BLOQUEADO 1
+#define DESBLOQUEADO 0
+
+/* Estrutura responsável por armazenar dados do Sieve */
 typedef struct tSieve{
     unsigned *valoresTestados;      /* BUFFER contendo todos os valores já testados */
     unsigned valorSerTestado;       /* valor a ser testado, se for igual a 0 não tem nenhum valor a ser testado */
 
     unsigned qntdValoresTestados;   /* contador para quantidade de valores testados */
-    unsigned posicao;               /* posicao do sieve na lista circular (identifica o sieve) */
+    unsigned posicao;               /* posicao do sieve no anel circular (identifica o sieve) */
+    short bloqueado;
 } Sieve;
-
+/* Estrutura que tem implementação parecida com uma lista circular */
 typedef struct tListaCircularSieve
 {
-    unsigned tamanhoMaximo;
-    unsigned tamanhoAtual;
-    int cursor;
-    Sieve *lista;
+    unsigned tamanhoMaximo;         /* tamanho máximo que a lista pode atingir */
+    Sieve *anel;                    /* anel conténdo todas as informações do sieves */
+    unsigned cursor;
 } ListaCircularSieve;
 
-ListaCircularSieve *criarLista(int tamanho);
+ListaCircularSieve *criar_Lista(int tamanho);
 void liberarLista(ListaCircularSieve *lista);
 
-bool adicionarElementoLista(ListaCircularSieve *sieves, unsigned posicao, unsigned elemento);
+void bloquearSieve(ListaCircularSieve *sieve, unsigned posicao);
+short verificarSieveDisponivel(ListaCircularSieve *sieve, unsigned posicao);
+short adicionarElementoLista(ListaCircularSieve *sieves, unsigned posicao, unsigned elemento);
 
-Sieve proximoDaLista(ListaCircularSieve *sieves, Sieve sieve){
-    unsigned posicao = sieve.posicao;
-    if(posicao+1 == sieves->tamanhoMaximo)
-        posicao = 0;
-    else
-        posicao--;
-    return sieves->lista[posicao];
-}
-
-bool listaCheia(ListaCircularSieve *sieves);
 void imprimirLista(ListaCircularSieve *sieves);
+
+short verificarSeElementosSaoDivisiveis(Sieve sieve, unsigned elementoComparado);
