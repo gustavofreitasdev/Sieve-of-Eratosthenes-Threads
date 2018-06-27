27fbd735ff0f4a6649019f200bcd931335194c4a
Gustavo F Silva <gustavofsilva.dev@gmail.com>
2018-06-27 03:04:40 -0300

    Criando arquivos para testes durante a produção.

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 default:
 	gcc -o exe main.c -pthread
 
+teste:
+	gcc -o exe main_teste.c -pthread
+
 clean:
 	rm exe
 
---
+++ main_teste.c
@@ -0,0 +1,32 @@
+#include "lista_circular_sieve.c"
+
+int main(void){
+    ListaCircularSieve *sieves = criarLista(8);
+    bloquearSieve(sieves, 0);
+    bloquearSieve(sieves, 3);
+    short teste = verificarSieveDisponivel(sieves, 0);
+    short teste2 = verificarSieveDisponivel(sieves, 1);
+    adicionarElementoLista(sieves, 0, 2);
+    adicionarElementoLista(sieves, 1, 3);
+    adicionarElementoLista(sieves, 0, 4);
+    printf("teste1 (block): %d, teste2(desbloc): %d\n", teste, teste2);
+
+
+    short teste3 = verificarSeElementosSaoDivisiveis(sieves->anel[0], 4);
+    short teste4 = verificarSeElementosSaoDivisiveis(sieves->anel[0], 2);
+    printf("teste3 (divi): %d, teste4 (n divi): %d\n", teste3, teste4);
+
+
+    imprimirLista(sieves);
+
+    desbloquearSieve(sieves, 0);
+    setValorSerTestado(sieves, 0, 6);
+    setValorSerTestado(sieves, 2, 8);
+    setValorSerTestado(sieves, 3, 9);
+    imprimirLista(sieves);
+
+    Sieve *sieve = getSieveDisponivelCalculo(sieves);
+    printf("Sieve disponivel: %d\n", sieve->posicao);
+
+    return 0;
+}
