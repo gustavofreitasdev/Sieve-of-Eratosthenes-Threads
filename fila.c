87b5f609c227f13fd2305a396360b7c7f8f77f90
André Paes <andrepaes@MacBook-Pro-de-Andre.local>
2018-06-27 05:14:27 -0300

    tad fila

---
+++ fila.c
@@ -0,0 +1,59 @@
+#include "fila_int.h"
+
+No *criarFila(){
+    No *fila;
+    fila = (No *)malloc(sizeof(No));
+    if(fila){
+        fila->prox = NULL;
+        fila->quantidade = 0;
+        return fila;
+    }else{
+        return 0;
+    }
+
+}
+
+void inserirFila(No *fila,int valor){
+    No *aux = fila->prox;
+    No *elemento = (No *)malloc(sizeof(No));
+    elemento->numero = valor;
+    if(elemento != NULL){
+        fila->prox = elemento;
+        elemento->prox = aux;
+        fila->quantidade++;
+    }
+}
+
+int retirarFila(No *fila){
+    No *aux = fila->prox;
+    int i,valor;
+    if(fila->quantidade == 1) {
+        valor = aux->numero;
+        free(fila->prox);
+        fila->prox = NULL;
+
+    }else if(fila->quantidade > 1){
+        No *aux2 = aux->prox;
+        for(i = 0 ; aux2->prox != NULL ; aux2 = aux2->prox , aux = aux->prox);
+        valor = aux2->numero;
+        free(aux->prox);
+
+        aux->prox = NULL;
+    }
+    fila->quantidade--;
+    printf("%d\n",valor);
+    return valor;
+}
+
+void main(){
+    No *fila;
+
+    int valor;
+    fila = criarFila();
+    inserirFila(fila,10);
+    inserirFila(fila,200);
+    inserirFila(fila,30);
+    valor = retirarFila(fila);
+    valor = retirarFila(fila);
+    valor = retirarFila(fila);
+}
---
+++ fila_int.h
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct tNo{
+    int numero;
+    struct tNo *prox;
+    int quantidade;
+}No;
+
